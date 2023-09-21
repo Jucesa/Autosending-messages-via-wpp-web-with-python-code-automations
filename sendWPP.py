4ba6d43148d472d396eb4832d5c2f9e5781633af
Jucesa <142596308+Jucesa@users.noreply.github.com>
2023-09-21 08:09:15 -0300

    sendWPP.py

--- sendWPP.py
+++ sendWPP.py
@@ -7,7 +7,7 @@ import gspread
 import time
 
 #conecting to spreadsheet in google sheets
-gc = gspread.service_account(filename=r'C://Users//jc160//OneDrive//Área de Trabalho//projeto de Programa, Contra evasão escolar//sendToResponsables//key.json')
+gc = gspread.service_account(filename='key.json')
 sheet = gc.open('spreadsheetdata').sheet1
 
 class message():
@@ -25,54 +25,63 @@ class message():
 
         #reading the data provided
         contatos = pd.read_csv(path)
-        print(contatos)
+        print(contatos) 
 
+        #declaring a dict object to store the data from the csv file
         data_dict = {}
 
-        # organizing the data for each student and verifying if a message was already sent
+        # organizing the data for each student
         for i, aluno in enumerate(contatos['Aluno']):
             responsavel = str(contatos.loc[i, 'Responsável'])
             numero = contatos.loc[i, 'Número']
             enviada = contatos.loc[i,'Enviada']
 
-            if enviada == False:
-                mensagem = str((f'Olá, {responsavel}, a Escola EREM Manoel Bacelar gostaria de saber os possíveis motivos da evasão do(a) aluno(a): {aluno}. Por favor, nos informe o quanto antes, agradecimentos, a direção.'))
+            mensagem = str((f'Olá, {responsavel}, a Escola EREM Manoel Bacelar gostaria de saber os possíveis motivos da evasão do(a) aluno(a): {aluno}. Por favor, nos informe o quanto antes, agradecimentos, a direção.'))
 
-                data = {'aluno':aluno,
-                        'reponsavel':responsavel,
+            data = {'aluno':aluno,
+                        'responsavel':responsavel,
                         'numero': numero,
                         'mensagem': mensagem,
                         'enviada': enviada}
                 
-                data_dict.setdefault(aluno,data)
-            else:
-                print(f'Mensagem já enviada para {responsavel}')
-                pass
+            data_dict.setdefault(aluno,data)
 
         return data_dict
     
     def createLink(data,aluno):
-
+        # Creating a custom link for each message
         phoneNumber = data[aluno]['numero']
         message = quote(data[aluno]['mensagem'])
 
         link = f"https://web.whatsapp.com/send/?phone={phoneNumber}&text={message}"
-        
         print(link)
-        return link
 
+        return link
+            
     def send(link,sheetRow):
+
         navegador.get(link)
         
         while len(navegador.find_elements(By.XPATH,'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span')) < 1:
             time.sleep(1)
 
         pyautogui.press('enter')
+
         sheet.update(f'D{sheetRow}',True)
+
         time.sleep(3)
 
 data_dict = message.getDataFromFile()
 navegador = webdriver.Chrome()
 
 for row,aluno in enumerate(data_dict.keys(),start=2):
-    message.send(message.createLink(data_dict,aluno),row)
+
+    # Verifying if a message was already sent
+    if data_dict[aluno]['enviada'] == False:
+
+        message.send(message.createLink(data_dict,aluno),row)
+        print(f'Enviando mensagem para {data_dict[aluno]["responsavel"]}')
+
+    else:
+
+        print(f'Mensagem já enviada para {data_dict[aluno]["responsavel"]}')
